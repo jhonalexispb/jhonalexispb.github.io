322cdf1f3ab5a0174cb6988102499d3e5967c2b1
jhonalexispb <143843282+jhonalexispb@users.noreply.github.com>
2024-01-17 12:26:08 -0500

    Creando mi primer app web

---
+++ name.js
@@ -0,0 +1,16 @@
+let username;
+
+function handleSubmit(event){
+    event.preventDefault();
+    let nombreUsuario = event.target.elements["newName"].value;
+    if(nombreUsuario!==""){
+    username = nombreUsuario;
+    window.location.href = "notas.html?newName=" + encodeURIComponent(username);
+    input.reset();
+}
+}
+const input = document.querySelector(".forName");
+input.addEventListener("submit",handleSubmit);
+
+
+
